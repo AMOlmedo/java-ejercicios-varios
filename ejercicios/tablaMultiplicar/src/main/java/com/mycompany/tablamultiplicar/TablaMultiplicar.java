21d0ed6ac11a7e1861753e8891f0690f00b8ac48
Adrian Olmedo <adrian.olmedo@hotmail.com>
2026-03-02 17:28:46 -0300

    fix tablamultiplicar

--- ejercicios/tablaMultiplicar/src/main/java/com/mycompany/tablamultiplicar/TablaMultiplicar.java
+++ ejercicios/tablaMultiplicar/src/main/java/com/mycompany/tablamultiplicar/TablaMultiplicar.java
@@ -1,18 +1,23 @@
 
-
 package com.mycompany.tablamultiplicar;
 
 import java.util.Scanner;
-
 public class TablaMultiplicar {
-
     public static void main(String[] args) {
         
         Scanner teclado = new Scanner(System.in );
-        
         int n;
+        System.out.println("ingrese el numero entero positivo: ");
+        n=teclado.nextInt();
+        if (n>0){
+            for(int i=1; i <=10;i++){
+                System.out.println(n + " x " + i + " = " + n*i);
+                
+            }
+        }else{
+            System.out.println("intentar nuevamente....");
+        }
         
-        System.out.println("ingrese el numero de la tabla que desea: ");
         
     }
 }
