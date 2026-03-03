e665c2141e439964426d80ee7a63b1fbc9323187
Adrian Olmedo <adrian.olmedo@hotmail.com>
2026-03-03 19:57:31 -0300

    fix *.java

--- ejercicios/tablaMultiplicar/src/main/java/com/mycompany/tablamultiplicar/TablaMultiplicar.java
+++ ejercicios/tablaMultiplicar/src/main/java/com/mycompany/tablamultiplicar/TablaMultiplicar.java
@@ -9,15 +9,17 @@ public class TablaMultiplicar {
         int n;
         System.out.println("ingrese el numero entero positivo: ");
         n=teclado.nextInt();
-        if (n>0){
-            for(int i=1; i <=10;i++){
+        boolean tester = true;
+        System.out.println("================");
+        while (tester == true){           
+            if (n>0){
+                for(int i=1; i <=10;i++){
                 System.out.println(n + " x " + i + " = " + n*i);
-                
+                }
+            }else{
+                System.out.println("intentar nuevamente....");
             }
-        }else{
-            System.out.println("intentar nuevamente....");
         }
-        
         
     }
 }
---
+++ ejercicios/tablaMultiplicar/src/main/java/com/mycompany/tablamultiplicar/tablasTodas.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tablamultiplicar;
+
+/**
+ *
+ * @author Adrian
+ */
+public class tablasTodas {
+    public static void main(String[] args) {
+        System.out.println("TABLAS DE MULTIPLICAR");
+        int n;
+        for (int i=0; i<=10; i++){
+            for (int j, j<=10: java.++){
+            System.out.println(" ");
+        }
+        }
+    
+    }
+}
