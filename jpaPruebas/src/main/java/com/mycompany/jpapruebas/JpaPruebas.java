be18a293ff615b809a6449adc2a0aa718786e811
Adrian Olmedo <adrian.olmedo@hotmail.com>
2026-03-04 19:42:16 -0300

    pruebas  en lab con jpa + postgresql

---
+++ jpaPruebas/src/main/java/com/mycompany/jpapruebas/JpaPruebas.java
@@ -0,0 +1,9 @@
+
+package com.mycompany.jpapruebas;
+
+public class JpaPruebas {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ jpaPruebas/src/main/java/com/mycompany/jpapruebas/logica/Alumnos.java
@@ -0,0 +1,52 @@
+
+package com.mycompany.jpapruebas.logica;
+
+
+public class Alumnos {
+    
+    private int id;
+    private String Nombre;
+    private String Apellido;
+    private String fechaNac;
+
+    public Alumnos(int id, String Nombre, String Apellido, String fechaNac) {
+        this.id = id;
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.fechaNac = fechaNac;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public String getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public void setFechaNac(String fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+    
+    
+}
