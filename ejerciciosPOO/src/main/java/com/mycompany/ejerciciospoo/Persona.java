6540702940b25fddf85ee93e397ac796ef5af414
Adrian Olmedo <adrian.olmedo@hotmail.com>
2026-03-05 18:09:32 -0300

    fix class Persona.java

--- ejerciciosPOO/src/main/java/com/mycompany/ejerciciospoo/Persona.java
+++ ejerciciosPOO/src/main/java/com/mycompany/ejerciciospoo/Persona.java
@@ -2,11 +2,16 @@
 package com.mycompany.ejerciciospoo;
 
 public class Persona {
+    //atributos o caracteristicas
     int  id;
     String nombre;
     String apellido;
     String direccion;
     int edad;
     
+    //metodos
+    //modificador de acceso 'public'
+    // void es un procedimiento no debuelva algo cmo un afuncion
+    public void mostrarNombre()
     
 }
