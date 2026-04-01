d27a7b0ef0895acc62c723ee828e2ef903115c58
Adrian Olmedo <adrian.olmedo@hotmail.com>
2026-04-01 11:27:58 -0300

    add list<>

--- EjVideoJuego/src/ejvideojuego/EjVideoJuego.java
+++ EjVideoJuego/src/ejvideojuego/EjVideoJuego.java
@@ -1,9 +1,27 @@
 // Ejercicio Integrador video Juegos TodoCode
 package ejvideojuego;
 
+import ejvideojuego.logica.VideoJuego;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EjVideoJuego {
 
     public static void main(String[] args) {
+      
+        List<VideoJuego> listaVideoJuegos = new ArrayList<VideoJuego>();
+        VideoJuego video1 = new VideoJuego(123,"banjo", "nintendo", 4, "plataforma" );
+        VideoJuego video2 = new VideoJuego(124, "pacman", "nintendo", 2, "plataforma");
+        VideoJuego video3 = new VideoJuego(125, "mario", "nintendo", 2, "plataforma");
+        VideoJuego video4 = new VideoJuego(126, "counter", "pc", 2, "shooter");
+        VideoJuego video5 = new VideoJuego(127, "age empira", "nintendo", 1, "estrategia");
+        
+        listaVideoJuegos.add(video1);
+        listaVideoJuegos.add(video2);
+        listaVideoJuegos.add(video3);
+        listaVideoJuegos.add(video4);
+        listaVideoJuegos.add(video5);
+        
         
     }
     
--- EjVideoJuego/src/ejvideojuego/logica/VideoJuego.java
+++ EjVideoJuego/src/ejvideojuego/logica/VideoJuego.java
@@ -8,5 +8,58 @@ public class VideoJuego {
     private String consola;
     private int cantJugadores;
     private String categoria;
+
+    public VideoJuego() {
+    }
+    
+    public VideoJuego(int codigo, String titulo, String consola, int cantJugadores, String categoria) {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.consola = consola;
+        this.cantJugadores = cantJugadores;
+        this.categoria = categoria;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getConsola() {
+        return consola;
+    }
+
+    public int getCantJugadores() {
+        return cantJugadores;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setConsola(String consola) {
+        this.consola = consola;
+    }
+
+    public void setCantJugadores(int cantJugadores) {
+        this.cantJugadores = cantJugadores;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    
+    
     
 }
