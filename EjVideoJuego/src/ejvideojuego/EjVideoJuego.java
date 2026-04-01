912412cd6cdd1e44a3d4e47c9916427d3b07f85a
Adrian Olmedo <adrian.olmedo@hotmail.com>
2026-04-01 11:07:42 -0300

    Ejercicio Integrador videojuego practica

---
+++ EjVideoJuego/src/ejvideojuego/EjVideoJuego.java
@@ -0,0 +1,10 @@
+// Ejercicio Integrador video Juegos TodoCode
+package ejvideojuego;
+
+public class EjVideoJuego {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ EjVideoJuego/src/ejvideojuego/logica/VideoJuego.java
@@ -0,0 +1,12 @@
+
+package ejvideojuego.logica;
+
+public class VideoJuego {
+    
+    private int codigo;
+    private String titulo;
+    private String consola;
+    private int cantJugadores;
+    private String categoria;
+    
+}
