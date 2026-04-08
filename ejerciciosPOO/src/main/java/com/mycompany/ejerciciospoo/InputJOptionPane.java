88f582e4c027d384256e729871227a0511809444
Adrian Olmedo <adrian.olmedo@hotmail.com>
2026-04-08 19:59:09 -0300

    exercise java JOPtionPane

---
+++ ejerciciosPOO/src/main/java/com/mycompany/ejerciciospoo/InputJOptionPane.java
@@ -0,0 +1,23 @@
+//ingresar datos utilizando JOptionPane
+package com.mycompany.ejerciciospoo;
+
+import javax.swing.JOptionPane;
+
+public class InputJOptionPane {
+    public static void main(String[] args) {
+        
+        String nombre;
+        int edad;
+        char genero;
+        double altura;
+        
+        nombre = JOptionPane.showInputDialog("Ingrese el nombre: ");
+        edad = Integer.parseInt(JOptionPane.showInputDialog("ingrese la edad: "));
+        genero = JOptionPane.showInputDialog("ingres H o M: ").charAt(0);
+        altura = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la altura en metros: "));
+        
+        System.out.println("tu nombre es " + nombre + " tienes " + edad + " años, tu genero es " + genero + " y tu altura es " + altura + " metros" );
+        
+    }
+    
+}
--- ejerciciosPOO/src/main/java/com/mycompany/ejerciciospoo/TipoDato.java
+++ ejerciciosPOO/src/main/java/com/mycompany/ejerciciospoo/TipoDato.java
@@ -15,7 +15,28 @@ public class TipoDato {
         //String es un tipo de dato NO PRIMITIVO. Es un wrapper y se comporta como un objeto
         //Tiene metodos
         
+        float altura;
+        System.out.println("Ingrese la altura: ");
+        altura = entrada.nextFloat();
+        
+        double peso;
+        System.out.println("Ingrese el peso: ");
+        peso = entrada.nextDouble();
+        
+        String pais;
+        System.out.println("Ingrese el pais de nacimiento: ");
+        pais = entrada.nextLine();
+                
+        char genero;
+        System.out.println("ingrese M o F: ");
+        genero = entrada.next().charAt(0);
+        
         System.out.println("la edad  de " + nombre  + " es " +edad);
+        System.out.println("la altura  de " + nombre  + " es " +altura);
+        System.out.println("la peso  de " + nombre  + " es " +peso);
+        System.out.println("la pais de recidencia  de " + nombre  + " es " +pais);
+        System.out.println("el genero de " + nombre  + " es " +genero);
+        
         
         
         
